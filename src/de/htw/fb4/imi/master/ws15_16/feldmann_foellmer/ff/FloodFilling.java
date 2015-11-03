9abfa4a77f7d20e5e02d62c47b8de463f8ddf6a8
Sascha Feldmann <sascha.feldmann@gmx.de>
2015-11-03 16:30:23 +0100

    refactored neighbour stuff

--- src/de/htw/fb4/imi/master/ws15_16/feldmann_foellmer/ff/FloodFilling.java
+++ src/de/htw/fb4/imi/master/ws15_16/feldmann_foellmer/ff/FloodFilling.java
@@ -10,32 +10,40 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
+import javax.jws.WebParam.Mode;
+
 /**
  * Implementation of flood filling.
  *
  * @since 27.10.2015
  */
 public class FloodFilling {
-	
+	public enum Mode {
+		NEIGHBOURS4, NEIGHBOURS8
+	}
+
 	private int[][] originalBinaryPixels;
 	private int[][] fillPixels;
-	
+
 	protected int width;
 	protected int height;
-	
-	
+
+	protected Mode mode = FloodFilling.Mode.NEIGHBOURS8;
+
 	public FloodFilling() {
-		
+
 	}
-	
+
 	public int[][] getOriginalPixels() {
 		return originalBinaryPixels;
 	}
 
 	/**
-	 * Set the original pixels by an 1-dimensional pixel array. 
+	 * Set the original pixels by an 1-dimensional pixel array.
 	 * 
-	 * Internally, the pixels are stored as 2-dimensional array. For conversion, hand in widht and height.
+	 * Internally, the pixels are stored as 2-dimensional array. For conversion,
+	 * hand in widht and height.
+	 * 
 	 * @param width
 	 * @param height
 	 * @param originalPixels
@@ -53,71 +61,94 @@ public class FloodFilling {
 			}
 		}
 	}
-	
+
+	public Mode getMode() {
+		return mode;
+	}
+
+	public void setMode(Mode mode) {
+		this.mode = mode;
+	}
+
 	private int calc1DPosition(int width, int x, int y) {
 		int pos = y * width + x;
 		return pos;
 	}
-	
+
 	/**
 	 * mit stack
 	 */
-	public void depthFirst(int u, int v, int label){		
-		Deque<Point> S = new LinkedList<Point>(); 	// create an empty stack stack S
-		S.push(new Point(u, v));					// push seed coordinate (u,v) onto S
+	public void depthFirst(int u, int v, int label) {
+		Deque<Point> S = new LinkedList<Point>(); // create an empty stack stack
+													// S
+		S.push(new Point(u, v)); // push seed coordinate (u,v) onto S
 		while (!S.isEmpty()) {
-			Point p = S.pop();						// pop first coordinate off the stack
+			Point p = S.pop(); // pop first coordinate off the stack
 			int x = p.x;
 			int y = p.y;
 			int pos = calc1DPosition(width, x, y);
-			
+
 			if ((x >= 0) && (x < width) && (y >= 0) && (y < height) && pos == 1) {
-//				ip.putPixel(x, y, label);
+				// ip.putPixel(x, y, label);
 				// kernelmitte (x,y) mit label zeichnen
-				
-				//noch vierer element!!!!
-//				S.push(new Point(x + 1, y));
-//				S.push(new Point(x, y + 1));
-//				S.push(new Point(x, y - 1));
-//				S.push(new Point(x - 1, y));
-				// kernel: achternachbarschaft
-				S.push(new Point(x + 1, y - 1));
-				S.push(new Point(x + 1, y));
-				S.push(new Point(x + 1, y + 1));
-				S.push(new Point(x, y + 1));
-				S.push(new Point(x, y - 1));
-				S.push(new Point(x - 1, y - 1));
-				S.push(new Point(x - 1, y));
-				S.push(new Point(x - 1, y + 1));
+
+				// noch vierer element!!!!
+				switch (mode) {
+					case NEIGHBOURS4:
+						push4Neighbour(S, x, y);
+						break;
+					case NEIGHBOURS8:
+						push8Neighbour(S, x, y);
+						break;
+				}
 			}
-		}	
+		}
 	}
-	
+
+	private void push4Neighbour(Deque<Point> S, int x, int y) {
+		S.push(new Point(x + 1, y));
+		S.push(new Point(x, y + 1));
+		S.push(new Point(x, y - 1));
+		S.push(new Point(x - 1, y));
+	}
+
+	private void push8Neighbour(Deque<Point> S, int x, int y) {
+		S.push(new Point(x + 1, y - 1));
+		S.push(new Point(x + 1, y));
+		S.push(new Point(x + 1, y + 1));
+		S.push(new Point(x, y + 1));
+		S.push(new Point(x, y - 1));
+		S.push(new Point(x - 1, y - 1));
+		S.push(new Point(x - 1, y));
+		S.push(new Point(x - 1, y + 1));
+	}
+
 	/**
 	 * mit queue
 	 */
 	public void breadthFirst(int u, int v, int label) {
-//		Queue<Point> Q = new LinkedList<Point>(); // queue Q
-//		Q.add(new Point(u, v));
-//		while (!Q.isEmpty()) {
-//			Point p = Q.remove(); // get the next point to process
-//			int x = p.x;
-//			int y = p.y;
-//			if ((x >= 0) && (x < width) && (y >= 0) && (y < height) && ip.getPixel(x, y) == 1) {
-//				ip.putPixel(x, y, label);
-//				//TODO : noch vierer element !!!!
-//				Q.add(new Point(x + 1, y));
-//				Q.add(new Point(x, y + 1));
-//				Q.add(new Point(x, y - 1));
-//				Q.add(new Point(x - 1, y));
-//			}
-//		}	
+		// Queue<Point> Q = new LinkedList<Point>(); // queue Q
+		// Q.add(new Point(u, v));
+		// while (!Q.isEmpty()) {
+		// Point p = Q.remove(); // get the next point to process
+		// int x = p.x;
+		// int y = p.y;
+		// if ((x >= 0) && (x < width) && (y >= 0) && (y < height) &&
+		// ip.getPixel(x, y) == 1) {
+		// ip.putPixel(x, y, label);
+		// //TODO : noch vierer element !!!!
+		// Q.add(new Point(x + 1, y));
+		// Q.add(new Point(x, y + 1));
+		// Q.add(new Point(x, y - 1));
+		// Q.add(new Point(x - 1, y));
+		// }
+		// }
 	}
-	
+
 	/**
 	 * Sequentielle Regionenmarkierung
 	 */
-	public void regionLabeling() {		
+	public void regionLabeling() {
 	}
 
 }
